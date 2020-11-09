59d324041c3f6a393421e0989f2d93eb73248949
leonardojacomussi <leonardo.jacomussi@eac.ufsm.br>
2020-11-09 02:19:17 -0300

    Git settings.

--- setup.py
+++ setup.py
@@ -9,6 +9,10 @@ Author:
 """
 
 from setuptools import setup
+import os
+
+path_setup = os.path.join(os.path.dirname(os.path.realpath('__file__')), 'pyslm')
+path_icon = os.path.join(path_setup, 'Icons')
 
 # with open("README.md", "r") as f:
 #     long_description = f.read()
@@ -16,7 +20,7 @@ from setuptools import setup
 settings = {
     'name': 'PySLM',
     'version': '1.2',
-    'description': 'Pythonic Sound Level Meter.',
+    'description': "Pythonic Sound Level Meter.",
     # 'long_description': long_description,
     # 'long_description_content_type': 'text/markdown',
     'url': 'https://github.com/leonardojacomussi/PySLM',
@@ -24,9 +28,10 @@ settings = {
     'author_email': 'leonardo.jacomussi@eac.ufsm.br',
     'license': 'MIT',
     'install_requires': ['numpy==1.19.1', 'scipy==1.5.0', 'matplotlib==3.3.1',
-            'pyqtgraph==0.11.0', 'sounddevice=0.4.0', 'PySide2==5.15.1', 'h5py==2.10.0'],
+                         'sounddevice==0.4.0', 'pyside2==5.15.1', 'h5py==2.10.0',
+                         'pyqtgraph==0.11.0'],
     'packages': ['pyslm'],
-    'package_dir': {'pyslm': 'pyslm'},
+    'package_dir': {'PySLM': 'pyslm'},
     'classifiers': [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
